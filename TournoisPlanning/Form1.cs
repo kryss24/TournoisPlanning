23509cdfc25fc3d12934262362eb98ac92ef34bc
kryss24 <113718014+kryss24@users.noreply.github.com>
2025-04-14 17:30:03 +0100

    Ajoutez des fichiers projet.

---
+++ TournoisPlanning/Form1.cs
@@ -0,0 +1,12 @@
+namespace TournoisPlanning
+{
+    public partial class Form1 : Form
+    {
+        Form2 form2 = new Form2();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
