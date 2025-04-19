116893051f05e9716870c4eb0d851f65581e15bb
kryss24 <113718014+kryss24@users.noreply.github.com>
2025-04-19 11:47:08 +0100

    Modify the type of project

---
+++ TournoisPlanning/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TournoisPlanning;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
